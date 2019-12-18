618cedd26b91e139a5918c2d112f58a82eef8754
Maxime Gavronsky <maximegavronsky@maximes-mbp.lan>
2019-12-17 22:14:50 -0500

    commiting progress before back end testing

--- server/app.py
+++ server/app.py
@@ -8,15 +8,25 @@ app = Flask(__name__)
 
 @app.route('/result', methods=['GET'])
 def result():
+    # ----------------- If no options are toggled -----------------
+    if not request.args['districts']:
+        res = app.response_class(
+            response=None,
+            status=200,
+            mimetype='application/json'
+        )
+        return res
+        
+    # ----------------- SIf at least one district is toggled -----------------
+    districts = request.args['districts'].lower().split('&')
+    for d in districts:
+        d.split()[0:-1]
 
-    district = request.args['district'].lower().split()[0:-1]
+    d_len = len(districts)
+    data = [None] * d_len
 
-    if 'Neighborhood' in district:
-        district = 'nta'
-    elif len(district) > 1:
-        district = district[1]
-
-    data = get_boundry(district)
+    for i in range(d_len):
+        data[i] = get_boundry(districts[i])
         
     res = app.response_class(
         response=json.dumps(data),
@@ -25,7 +35,14 @@ def result():
     )
     return res
 
+#---------- Retrieve district geo info ---------------
 def get_boundry(repo_name):
+
+    if 'neighborhood' in repo_name:
+        repo_name = 'nta'
+    elif len(repo_name) > 1:
+        repo_name = repo_name[1]
+
     client = pymongo.MongoClient()
     repo = client.district_repo
     data = repo[repo_name].find_one()
